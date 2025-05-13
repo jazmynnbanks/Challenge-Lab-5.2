41a2e2f99e364ed0bf3c2667745a4eee4ef73d5b
jazmynnbanks <jazmynn.lee@outlook.com>
2025-05-13 17:20:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int[] myArray = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+int n = myArray.Length;
+int expectedSum = n * (n + 1) / 2;
+
+int actualSum = 0;
+
+foreach (int i in myArray)
+{
+    actualSum += i;
+}
+
+int missingNumber = expectedSum -  actualSum;
+Console.WriteLine(missingNumber);
